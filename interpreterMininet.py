5167db09de796e85b2274ddd75112ae076f7f515
cristianvelascofm <cfvelasco@unicauca.edu.co>
2021-02-05 15:54:46 -0500

    Server Mininet Alpha 0.0.22

--- interpreterMininet.py
+++ interpreterMininet.py
@@ -541,9 +541,8 @@ def interpreter(json_data, connection):
         for name in file_traffic:
             file_name = str(name)
             
-            data_gen['local_host'] = data_traffic[file_name]['start']
-            '''['connected']['local_host']
-            data_gen['remote_host'] =data_traffic[file_name]['start']['connected']['remote_host']'''
+            data_gen['local_host'] = data_traffic[file_name]['start']['local_host']['connectig_to']['host']
+            data_gen['remote_host'] =data_traffic[file_name]['start']['local_host']['connected']['local_host']
             procces_data[file_name]= data_gen
             #algo = data_traffic[file_name]['start']['connecting_to']['host']
         print('algo: ',procces_data)
