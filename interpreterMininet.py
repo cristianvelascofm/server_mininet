0ee3b23e574d7cc2f6753617b7acff124d60389e
cristianvelascofm <cfvelasco@unicauca.edu.co>
2021-02-11 10:27:49 -0500

    Server Mininet Alpha 0.15

--- interpreterMininet.py
+++ interpreterMininet.py
@@ -539,6 +539,7 @@ def interpreter(json_data, connection):
 
         for name in name_files:
             archive_json = json.loads(open(str(name)+'.json').read())
+            time.sleep(5)
             dict_data_traffic[str(name)] = archive_json
 
         #print(dict_data_traffic)
